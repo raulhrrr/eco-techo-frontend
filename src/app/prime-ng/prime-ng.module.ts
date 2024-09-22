7809d4aab12db2ece79f637d029160e783633316
Raul Herrera <raulherrera01022003@gmail.com>
2024-09-22 15:43:13 -0500

    Removed innecesary modules

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,28 +1,16 @@
 import { NgModule } from '@angular/core';
 
-import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
-import { TableModule } from 'primeng/table';
-import { ToolbarModule } from 'primeng/toolbar';
 import { ChartModule } from 'primeng/chart';
-import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
 
 @NgModule({
   exports: [
-    ButtonModule,
-    CardModule,
-    FieldsetModule,
-    MenubarModule,
     PanelModule,
-    TableModule,
-    ToolbarModule,
+    CardModule,
     ChartModule,
-    InputTextModule,
-    PasswordModule
+    MenubarModule,
   ]
 })
 export class PrimeNgModule { }
