1e08bf9b1dc2de1d2b9a1e397092cbcf325ec2a6
Raul Herrera <raulherrera01022003@gmail.com>
2024-10-07 22:39:44 -0500

    Refactor date-utils to handle inputDate as string

--- src/app/utils/date-utils.ts
+++ src/app/utils/date-utils.ts
@@ -1,6 +1,8 @@
 import { groupBy } from "../dashboard/interfaces";
 
 export function getFormattedDate(inputDate?: Date): string {
+  if (typeof inputDate === 'string') return inputDate;
+
   const date = inputDate ?? new Date();
   const yyyy = date.getFullYear();
   const mm = String(date.getMonth() + 1).padStart(2, '0');
@@ -11,9 +13,9 @@ export function getFormattedDate(inputDate?: Date): string {
 
 export function getDayOrHourFromDate(date: string, groupBy: groupBy): string {
   if (groupBy === 'day') {
-    return date.split('T')[0];
+    return date.split(' ')[0];
   }
 
-  const hourSplited = date.split('T')[1].split(':');
+  const hourSplited = date.split(' ')[1].split(':');
   return `${hourSplited[0]}:${hourSplited[1]}`;
 }
