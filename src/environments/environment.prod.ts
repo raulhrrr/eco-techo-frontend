8b214ca77c30895c6c96ea9e09b28e0cd491c98d
Raul Herrera <raulherrera01022003@gmail.com>
2024-10-13 19:08:09 -0500

    Refactor build configuration and environment setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  baseUrl: 'https://eco-techo-backend-production.up.railway.app',
+  isAuthEnabled: false,
+  socketUrl: 'wss://eco-techo-backend-production.up.railway.app',
+};
